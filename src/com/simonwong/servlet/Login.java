9d28c7be7d9d59308ea5dab355fc97c4f30edad3
SimonWong <codersimonwong@gmail.com>
2020-09-23 17:57:13 +0800

    简单请求

--- src/com/simonwong/servlet/Login.java
+++ src/com/simonwong/servlet/Login.java
@@ -12,12 +12,14 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("get");
+        doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("post");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.getWriter().write("success");
     }
 
 }
