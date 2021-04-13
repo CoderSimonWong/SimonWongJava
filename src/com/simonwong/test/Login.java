33307c4ade1fb3335c3afa13382e89afb7a9991d
SimonWong <389200392@qq.com>
2021-04-13 19:01:22 +0800

    1

---
+++ src/com/simonwong/test/Login.java
@@ -0,0 +1,26 @@
+package com.simonwong.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.setHeader("Content-Type", "application/json;charset=UTF-8");
+        resp.getWriter().write("{\"code\":\"20000\", \"message\":\"请求成功\"}");
+    }
+
+}
